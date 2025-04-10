0f169544f6c29c7b01c795bb0e37674225e9c1a2
saniya0729 <saniyakhan0729>
2025-04-10 21:13:20 +0530

    "changes from laptop"

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,31 +1,44 @@
-import seriesData from "../api/seriesData.json"
-const NetflixSeries=()=>{
-    return(
-       <ul>
-        { 
-            seriesData.map((curElem)=>{
-              return(  
-              <li>
-                    <div>
-             <img src={curElem.img_url} alt="" width="40%" height="40%"/>
-         </div>
-         <h2>Name: {curElem.name}</h2>
-         <h3>Ratings: {curElem.rating}</h3>
-         <p>Summary: {curElem.description}</p>
-        <p>Genre: {curElem.genre}</p>
-         <p>Cast: {curElem.cast }</p>
-         <a href={curElem.Watch_url} target="_black">
-            <button>Watch Now</button>
-        </a>
-                </li>
-            )
-            })
-        }
-       </ul>
-    )
-}
+import seriesData from "../api/seriesData.json";
+import { SeriesCard } from "./SeriesCard";
+
+const NetflixSeries = () => {
+    return (
+        <ul className="grid grid-three--cols">
+            {seriesData.map((curElem) => {
+                return (
+                    <SeriesCard key={curElem.id} data ={curElem}/>
+                )
+            })}
+        </ul>
+    );
+};
 export default NetflixSeries;
 
+// import seriesData from "../api/seriesData.json";
+// const NetflixSeries = () => {
+//     return (
+//         <ul>
+//             {seriesData.map((curElem) => {
+//                 return (
+//                     <li>
+//                         <div>
+//                             <img src={curElem.img_url} alt="" width="40%" height="40%" />
+//                         </div>
+//                         <h2>Name: {curElem.name}</h2>
+//                         <h3>Ratings: {curElem.rating}</h3>
+//                         <p>Summary: {curElem.description}</p>
+//                         <p>Genre: {curElem.genre}</p>
+//                         <p>Cast: {curElem.cast}</p>
+//                         <a href={curElem.Watch_url} target="_black">
+//                             <button>Watch Now</button>
+//                         </a>
+//                     </li>
+//                 );
+//             })}
+//         </ul>
+//     );
+// };
+// export default NetflixSeries;
 
 // import seriesData from "../api/seriesData.json"
 // const NetflixSeries=()=>{
@@ -47,7 +60,6 @@ export default NetflixSeries;
 // }
 // export default NetflixSeries;
 
-
 // const NetflixSeries = () => {
 //     const name = "My Demon";
 //     const rating = 9.8;
@@ -60,18 +72,18 @@ export default NetflixSeries;
 //     let age = 18;
 //     // let canWatch = "Not Available";
 //     // if(age>=18) canWatch="Watch Now"
-    
+
 //     const canWatch=()=>{
 //     if(age>=18) return "Watch Now";
 //     return "Not Available"
 //   }
-  
+
 //     // if (age < 18) {
 //     //   return (
 //     //     <div>
 //     //       <div>
 //     //         <img src="https://i0.wp.com/www.poptokki.com/wp-content/uploads/2024/01/GEFhLwxbkAAVXd3.jpg?resize=1024%2C1024&ssl=1" width="40%" height="40%" alt="" />
-  
+
 //     //       </div>
 //     //       <h2>Name : {name}</h2>
 //     //       <h3>Rating: {5 + 4.8}</h3>
@@ -81,13 +93,12 @@ export default NetflixSeries;
 //     //     </div>
 //     //   )
 //     // }
-    
-     
+
 //       return (
 //         <div>
 //           <div>
 //             <img src="https://i0.wp.com/www.poptokki.com/wp-content/uploads/2024/01/GEFhLwxbkAAVXd3.jpg?resize=1024%2C1024&ssl=1" width="40%" height="40%" alt="" />
-    
+
 //           </div>
 //           <h2>Name : {name}</h2>
 //           <h3>Rating: {5 + 4.8}</h3>
@@ -108,4 +119,3 @@ export default NetflixSeries;
 //   export const Footer =()=>{
 //     return <p>Copyright @thapatechnical</p>
 //   }
-
---
+++ src/components/SeriesCard.jsx
@@ -0,0 +1,89 @@
+import styles from "./Netflix.module.css";
+
+// Destructuring in jsx
+export const SeriesCard = ({ data }) => {
+  const { img_url, name, rating, description, genre, cast, Watch_url } = data;
+  // inline css
+  const btn_style = {
+    padding: "1.2rem 2.4rem",
+    border: "none",
+    fontSize: "1.6rem",
+    backgroundColor: `${
+      rating >= 8.5 ? "rgb(7, 216, 129)" : "rgb(233, 218, 7)"
+    }`,
+    color: "var(--heading-color: #000a19;)",
+  };
+  const rating_style = {
+    fontSize: "1.5rem",
+  };
+  // const ratingClass = rating >= 8.5 ? "super_hit": "average";
+  const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
+  return (
+    <li className={styles.card}>
+      <div>
+        <img src={img_url} alt="" />
+      </div>
+      <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+      <div className={styles["card-content"]}>
+        <h2>Name: {name}</h2>
+        {
+          <h5>
+            Ratings:{" "}
+            <span className={`${styles.rating} ${ratingClass}`}>{rating}</span>
+          </h5>
+          // { <h5>Ratings: <span className= {`rating ${rating >= 8.5 ? "super_hit": "average"}`}>{rating}</span></h5>
+        }{" "}
+        {/* inline css */}
+        <p style={{ margin: "1.2red 0" }}>Summary: {description}</p>
+        <p>Cast: {cast}</p>
+        <a href={Watch_url} target="_black">
+          <button style={btn_style}>Watch Now</button>
+          {/* <button style={{
+                        padding: "1.2rem 2.4rem",
+                        border: "none",
+                        fontSize: "1.6rem",
+                        backgroundColor: "var(--btn-hover-bg-color)",
+                        color: "var(--bg-color)"
+                    }}>Watch Now</button> */}
+        </a>
+      </div>
+    </li>
+  );
+};
+// export const SeriesCard = (props) => {
+//     // const {data}= props;
+
+//     return (
+//         <li>
+//             <div>
+//                 <img src={data.img_url} alt="" width="40%" height="40%" />
+//             </div>
+//             <h2>Name: {data.name}</h2>
+//             <h3>Ratings: {data.rating}</h3>
+//             <p>Summary: {data.description}</p>
+//             <p>Genre: {data.genre}</p>
+//             <p>Cast: {data.cast}</p>
+//             <a href={data.Watch_url} target="_black">
+//                 <button>Watch Now</button>
+//             </a>
+//         </li>
+//     );
+// }
+
+// export const SeriesCard = (props) => {
+//     return (
+//         <li>
+//             <div>
+//                 <img src={props.curElem.img_url} alt="" width="40%" height="40%" />
+//             </div>
+//             <h2>Name: {props.curElem.name}</h2>
+//             <h3>Ratings: {props.curElem.rating}</h3>
+//             <p>Summary: {props.curElem.description}</p>
+//             <p>Genre: {props.curElem.genre}</p>
+//             <p>Cast: {props.curElem.cast}</p>
+//             <a href={props.curElem.Watch_url} target="_black">
+//                 <button>Watch Now</button>
+//             </a>
+//         </li>
+//     );
+// }
